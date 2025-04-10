015608dfce92ebc767c9675fc26863c77a24e0f7
nafimbrk <nafimubarok2005@gmail.com>
2025-04-10 04:22:37 -0700

    route

--- src/js/routes.js
+++ src/js/routes.js
@@ -1,10 +1,20 @@
 
 import HomePage from '../pages/home.f7';
+import Profil from '../pages/profil.f7';
+import Kontak from '../pages/kontak.f7';
 
 var routes = [
   {
     path: '/',
     component: HomePage,
+  },
+  {
+    path: '/profil/',
+    component: Profil,
+  },
+  {
+    path: '/kontak/',
+    component: Kontak,
   },
 ];
 
