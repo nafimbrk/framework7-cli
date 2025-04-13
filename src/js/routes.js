dcbae044a4f9a16b677a44d4724c95dece947e5d
nafimbrk <nafimubarok2005@gmail.com>
2025-04-13 08:55:52 -0700

    get json

--- src/js/routes.js
+++ src/js/routes.js
@@ -3,6 +3,9 @@ import HomePage from '../pages/home.f7';
 import Profil from '../pages/profil.f7';
 import Kontak from '../pages/kontak.f7';
 
+import Matematika from '../pages/matematika.f7';
+import Diskon from '../pages/diskon.f7';
+
 var routes = [
   {
     path: '/',
@@ -15,6 +18,14 @@ var routes = [
   {
     path: '/kontak/',
     component: Kontak,
+  },
+  {
+    path: '/matematika/',
+    component: Matematika,
+  },
+  {
+    path: '/diskon/',
+    component: Diskon,
   },
 ];
 
