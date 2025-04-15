125f86d1ceacceea28ab04233a369f2aa6d45a8d
nafimbrk <nafimubarok2005@gmail.com>
2025-04-15 06:32:07 -0700

    selesai sudah

--- src/js/routes.js
+++ src/js/routes.js
@@ -6,6 +6,9 @@ import Kontak from '../pages/kontak.f7';
 import Matematika from '../pages/matematika.f7';
 import Diskon from '../pages/diskon.f7';
 
+import TambahPage from '../pages/tambah.f7';
+import EditPage from '../pages/edit.f7';
+
 var routes = [
   {
     path: '/',
@@ -26,6 +29,14 @@ var routes = [
   {
     path: '/diskon/',
     component: Diskon,
+  },
+  {
+    path: '/tpegawai/',
+    component: TambahPage,
+  },
+  {
+    path: '/edit/:id/',
+    component: EditPage,
   },
 ];
 
